32248299a2737a46f386f3071024259425c6f00c
yhn <yhn@evecom.net>
2021-04-25 16:30:18 +0800

    parcel实现简单搭建

---
+++ src/button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="g-button">hi</button>
+</template>
+
+<script>
+export default {
+  name: 'g-button'
+}
+
+</script>
+
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    &:hover {
+      color: red;
+    }
+  }
+</style>
