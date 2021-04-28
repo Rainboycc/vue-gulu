dc00a7bf80a60f981a2ec6058fc898cece7a95ef
yhn <yhn@evecom.net>
2021-04-28 17:04:02 +0800

    封装单页面组件Button

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="g-button">hi</button>
+  <button class="g-button">按钮</button>
 </template>
 
 <script>
@@ -13,8 +13,18 @@ export default {
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
     &:hover {
-      color: red;
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
     }
   }
 </style>
