e24a9af173c6714746015846bd2c3fb21faaa1bd
yhn <yhn@evecom.net>
2021-05-07 16:34:16 +0800

    发包配置

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 import Icon from './src/icon.vue'
-
-export default { Button, ButtonGroup, Icon }
+export { Button, ButtonGroup, Icon }
