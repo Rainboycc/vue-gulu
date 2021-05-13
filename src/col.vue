90cddbb3b4b2b1c37915e2b432795b8e81bfdad5
yhn <yhn@evecom.net>
2021-05-13 22:51:37 +0800

    网格系统实现offset 和 gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" 
+    :class="[span && `col-${span}`, offset && `offset-${offset}`]" 
+    :style="{ paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+    <div style="border: 1px solid green; height: 100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,6 +14,14 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0
       }
     }
   }
@@ -17,14 +29,17 @@
 
 <style lang="scss" scoped>
   .col {
-    height: 100px;
-    background: grey;
     width: 50%;
-    border: 1px solid red;
     $class-prefix: col-;
     @for $i from 1 through 24 {
       &.#{$class-prefix}#{$i} {
         width: ($i / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $i from 1 through 24 {
+      &.#{$class-prefix}#{$i} {
+        margin-left: ($i / 24) * 100%;
       }
     }
   }
