5f5fed8627f8d0e59fbbf3b032dc8f1221319c2a
yhn <yhn@evecom.net>
2021-05-13 22:12:44 +0800

    ro w和col的雏形雕琢完成

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import Button from './button.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component(Button.name, Button)
 Vue.component(ButtonGroup.name, ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 new Vue ({
   el: '#app',
   data: {
